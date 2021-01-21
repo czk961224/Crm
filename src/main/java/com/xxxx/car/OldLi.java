f04214fa226513f7f6a9bfd0a84d820d40b7eb65
czk <1344689554@qq.com>
2021-01-21 18:22:52 +0800

    第一次提交

---
+++ src/main/java/com/xxxx/car/OldLi.java
@@ -0,0 +1,8 @@
+package com.xxxx.car;
+
+public class OldLi implements InterCar{
+    @Override
+    public void behaviour() {
+        System.out.println("奈斯啊 老李开车了");
+    }
+}
---
+++ src/main/java/com/xxxx/cglib/Cglib1.java
@@ -0,0 +1,10 @@
+package com.xxxx.cglib;
+
+/**
+ * 目标对象
+ */
+public class Cglib1 {
+    public void toRen(){
+        System.out.println("开车了 嘀嘀嘀");
+    }
+}
