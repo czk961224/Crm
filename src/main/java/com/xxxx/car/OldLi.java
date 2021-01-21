4f3bf3cd385b847510290736f2eea63beda2d8ce
czk <1344689554@qq.com>
2021-01-21 18:39:50 +0800

    dev分支初始化操作第二次

--- src/main/java/com/xxxx/car/OldLi.java
+++ src/main/java/com/xxxx/car/OldLi.java
@@ -5,5 +5,6 @@ public class OldLi implements InterCar{
     public void behaviour() {
         System.out.println("奈斯啊 老李开车了dev 分支操作");
         System.out.println("就这样把dev");
+        System.out.println("啊 老李开车了dev 分");
     }
 }
